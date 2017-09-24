05ecbd1ca87b4da079f2b084cdaf5263c0d807ef
Mankiran kaur <mankirankaur@Mankirans-MacBook-Pro.local>
2017-09-24 12:44:09 -0400

    displaying numbers on screen

--- MAPD-Calculator-Assignment1/ViewController.swift
+++ MAPD-Calculator-Assignment1/ViewController.swift
@@ -9,6 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var numberondisplay:Double = 0;
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func numbers(_ sender: UIButton)
+    {
+        label.text = label.text! + String(sender.tag-1)
+        numberondisplay = Double(label.text!)!
+        
+    }
+    
+    @IBAction func operations(_ sender: UIButton)
+    {
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
