6861c0b7884ec2965ef9f0d851c6757146b780ad
Mankiran kaur <mankirankaur@Mankirans-MacBook-Pro.local>
2017-09-25 23:14:26 -0400

    added decimal function and clear button working properly

--- MAPD-Calculator-Assignment1/ViewController.swift
+++ MAPD-Calculator-Assignment1/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     var othernumber : Double = 0;
     var oprator : String = ""
     var result : Double = 0;
+    var decimalflag: Bool = false
+    
     
     @IBOutlet weak var label: UILabel!
     
@@ -21,29 +23,53 @@ class ViewController: UIViewController {
     {
         if result == 0
         {
-            if label.text == "+"{
+            if label.text == "+"    {
                 label.text = String(sender.tag)
             }
-            else if label.text == "-"{
+            else if label.text == "-"   {
                 label.text = String(sender.tag)
             }
-            else if label.text == "x"{
+            else if label.text == "x"   {
                 label.text = String(sender.tag)
             }
-            else if label.text == "÷"{
+            else if label.text == "÷"   {
                 label.text = String(sender.tag)
             }
             else {
-                label.text = label.text! + String(sender.tag)
+                if label.text == String(0)  {
+                    if sender.tag == 17{
+                        if decimalflag == false {
+                            label.text = "0."
+                            decimalflag = true
+                        }
+                    }
+                    else    {
+                        
+                        label.text = String(sender.tag)
+                    }
+                    
+                }
+                else    {
+                    if(sender.tag == 17)    {
+                        if decimalflag == false {
+                            label.text = label.text! + "."
+                            decimalflag = true
+                        }
+                    }
+                    else    {
+                        label.text = label.text! + String(sender.tag)
+                    }
+                    
+                }
             }
             
         }
         else  {
-            label.text = ""
-            result = 0;
-            numberondisplay = 0;
-            othernumber = 0;
-            oprator = ""
+            //label.text = ""
+            //result = 0;
+            //numberondisplay = 0;
+            //othernumber = 0;
+            //oprator = ""
             
             if label.text == "+"{
                 label.text = String(sender.tag)
@@ -58,50 +84,78 @@ class ViewController: UIViewController {
                 label.text = String(sender.tag)
             }
             else {
-                label.text = label.text! + String(sender.tag)
+                if label.text == String(0)  {
+                    if sender.tag == 17 {
+                        label.text = "0."
+                        decimalflag = true
+                    }
+                    else    {
+                        label.text = String(sender.tag)
+                    }
+                }
+                else    {
+                    if(sender.tag == 17)    {
+                        if decimalflag == false {
+                            label.text = label.text! + "."
+                            decimalflag = true
+                        }
+                    }
+                    else    {
+                        label.text = label.text! + String(sender.tag)
+                    }
+                }
+                
             }
-            
         }
-        
     }
     
     @IBAction func operations(_ sender: UIButton)
     {
-        if label.text != ""
-        {
-            if sender.tag == 12{
+        //if label.text != ""
+       // {
+            if sender.tag == 12 {                    //divide
                 numberondisplay = Double(label.text!)!
                 oprator = "÷"
                 label.text = "÷"
+                decimalflag = false
             }
-            else if sender.tag == 13{
+            else if sender.tag == 13    {
                 numberondisplay = Double(label.text!)!
                 oprator = "x"
                 label.text = "x"
+                decimalflag = false
             }
-            else if sender.tag == 14{
+            else if sender.tag == 14    {               //minus
                 numberondisplay = Double(label.text!)!
                 oprator = "-"
                 label.text = "-"
+                decimalflag = false
+                
             }
-            else if sender.tag == 15{
+            else if sender.tag == 15    {               // plus
                 numberondisplay = Double(label.text!)!
                 oprator = "+"
                 label.text = "+"
+                decimalflag = false
             }
-            else if sender.tag == 11{
+            else if sender.tag == 11    {     //clear operation
                 numberondisplay = 0
                 othernumber = 0
                 oprator = ""
-                label.text = ""
+                label.text = "0"
+                decimalflag = false
             }
-            else if sender.tag == 16{
-                if numberondisplay != 0{
+            else if sender.tag == 16
+            {
+                if numberondisplay != 0
+                {
                     othernumber = Double(label.text!)!
+                    print("numberonDisplay - \(numberondisplay)")
+                    print("othernumber- \(othernumber)")
                     if oprator == "+"{
                         result = numberondisplay + othernumber
                         label.text = String(result)
-                        }
+                    }
                     else if oprator == "-"{
                         result = numberondisplay - othernumber
                         label.text = String(result)
@@ -114,15 +168,13 @@ class ViewController: UIViewController {
                         result = numberondisplay / othernumber
                         label.text = String(result)
                     }
-                    
-                    
-                    
+                    numberondisplay = result
+                   // print(numberondisplay)
                 }
+                
             }
         
-        }
-        
-        
+        //}
         
     }
     
